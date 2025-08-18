da58b0356ce809e5583425439bd979a2648ee27d
jeffsirocki <jsirocki@gmail.com>
2025-08-18 17:13:07 -0400

    method_missing for ends_with(?)

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   ACTIONS.each_with_index do |action, i|
     define_method(action) do
       self.decrement!(:stock) if i.zero?
-      self.increment!(:stock) if i == 1
+      self.increment!(:stock) if action = "return"
       puts "#{action.capitalize} 1 product"
     end
   end
@@ -13,5 +13,14 @@ class Product < ApplicationRecord
     return unless self.respond_to?(action)
 
     item.send(action)
+  end
+
+  def method_missing(method_name, *args)
+    if method_name.ends_with?("?")
+      result = self.send(method_name.chop, *args)
+      result.present? && result != 0
+    else
+      super
+    end
   end
 end
